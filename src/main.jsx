77a73ac35aec5f03e78d4d16b5f9f53645824aef
GeekWolf007 <geeksingal@gmail.com>
2023-11-15 17:40:18 +0530

    hamburger component

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,18 @@ const router = createBrowserRouter([
     path: '/home',
     element: <h1>home</h1>,
   },
+  {
+    path: '/login',
+    element: <h1>login</h1>,
+  },
+  {
+    path: '/events',
+    element: <h1>events</h1>,
+  },
+  {
+    path: '/clubs',
+    element: <h1>clubs</h1>,
+  },
   //....... add more routes 
 ])
 
