1bffd407cb580a955d0645e398c05984f6685ec8
GeekWolf007 <geeksingal@gmail.com>
2023-11-19 03:43:35 +0530

    add: scroll down

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -3,12 +3,18 @@ import Header from '../components/Header'
 import HeroSection from '../components/HeroSection'
 import '../styles/landingpage.css'
 
+import scrollDown from "../assets/scrolldown_icon.svg"
+
 function LandingPage() {
   return (
     <React.Fragment>
       <div className="landing-bg">
         <Header />
         <HeroSection />
+        <div className="scroll-down">
+          <span>Scroll</span>
+          <img src={scrollDown} alt="down" />
+        </div>
       </div>
     </React.Fragment>
   )
