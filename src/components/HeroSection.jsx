0f0be07b986b5f1fa0c15c7724c124ffede1b084
GeekWolf007 <geeksingal@gmail.com>
2023-11-19 02:03:41 +0530

    add: hero section non interactive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,26 +2,44 @@ import React from 'react'
 import HeroSectionLink from '../helpers/HeroSectionLink'
 import '../styles/herosection.css'
 
+import rightIcon from '../assets/right_arrow_icon.svg'
+import profileIcon from '../assets/profile_icon.svg'
+import eventsIcon from '../assets/events_icon.svg'
+import recruitmentsIcon from '../assets/recruitments_icon.svg'
+
 function HeroSection() {
   return (
     <React.Fragment>
       <div className="hero-bg">
         <HeroSectionLink
+          bgImage
+          text={'Clubs & Assocs'}
+          textColor={'#FFFFFF'}
+          icon={rightIcon}
           backgroundColor="#2541B2"
           gridArea=" 1 / 1 / 3 / 3"
-          square
         />
         <HeroSectionLink
+          text={'Events'}
+          textColor={'#000000'}
+          icon={eventsIcon}
           backgroundColor="#ECECEC"
           gridArea=" 1 / 3 / 2 / 4"
-          square
         />
         <HeroSectionLink
+          text={'Profile'}
+          textColor={'#FFFFFF'}
+          icon={profileIcon}
           backgroundColor="#C13E42"
           gridArea=" 1 / 4 / 2 / 5"
-          square
         />
-        <HeroSectionLink backgroundColor="#1C976B" gridArea=" 2 / 3 / 3 / 5" />
+        <HeroSectionLink
+          text={'recruitments'}
+          textColor={'#FFFFFF'}
+          icon={recruitmentsIcon}
+          backgroundColor="#1C976B"
+          gridArea=" 2 / 3 / 3 / 5"
+        />
       </div>
     </React.Fragment>
   )
--- src/helpers/HeroSectionLink.jsx
+++ src/helpers/HeroSectionLink.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import '../styles/herosection.css'
+import bgImageIcons from '../assets/Clubs&Assocs.svg'
 
-function HeroSectionLink({ backgroundColor, gridArea, square }) {
-  const style = {
+function HeroSectionLink({
+  backgroundColor,
+  gridArea,
+  bgImage,
+  text,
+  // textHover,
+  textColor,
+  icon,
+}) {
+  
+	const [isHovered, setIsHovered] = useState(false)
+	
+	const style = {
     backgroundColor: backgroundColor,
     gridArea: gridArea,
-    aspectRatio: square ? '1' : 'unset',
+    backgroundImage: bgImage ? `url(${bgImageIcons})` : 'none',
+		cursor: isHovered ? 'pointer' : 'default',
   }
+
 
   return (
     <React.Fragment>
-      <div className="hero-link" style={style}></div>
+      <div
+        className="hero-link"
+        style={style}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {!isHovered && (
+          <div className="hero-link-label">
+            <p style={{ color: textColor }}>{text}</p>
+            <img src={icon} alt="icon" />
+          </div>
+        )}
+      </div>
     </React.Fragment>
   )
 }
@@ -21,5 +47,9 @@ export default HeroSectionLink
 HeroSectionLink.propTypes = {
   backgroundColor: PropTypes.string,
   gridArea: PropTypes.string,
-  square: PropTypes.bool,
+  bgImage: PropTypes.bool,
+  text: PropTypes.string,
+  textHover: PropTypes.string,
+  textColor: PropTypes.string,
+  icon: PropTypes.string,
 }
