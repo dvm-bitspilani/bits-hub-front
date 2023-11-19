2f8c1920400fe80ef9b147082ff235fbc285a1a4
GeekWolf007 <geeksingal@gmail.com>
2023-11-20 02:14:46 +0530

    add: home page navigation on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import '../styles/header.css'
 
 import searchIcon from '../assets/search_icon.svg'
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
   return (
     <React.Fragment>
       <div className="header-bg">
@@ -12,7 +19,7 @@ function Header() {
           <div className="hamline2"></div>
           <div className="hamline3"></div>
         </div>
-        <h1>BITS Hub</h1>
+        <h1 onClick={handleClick}>BITS Hub</h1>
         <img src={searchIcon} alt="Search" />
       </div>
     </React.Fragment>
